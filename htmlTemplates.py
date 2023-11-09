f3071816c0f30fc5ff182357cb29f8a1e9c788cb
PruekRp <pruek.rp@gmail.com>
2023-11-09 21:55:24 +0700

    first commit

---
+++ htmlTemplates.py
@@ -0,0 +1,50 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; 
+    border-radius: 0.5rem; 
+    margin-bottom: 1rem; 
+    display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar.user {
+  width: 20%;
+}
+.chat-message .avatar.bot {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar bot">
+        <img src="https://img.icons8.com/ios-filled/50/retro-robot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar user">
+        <img src="https://img.icons8.com/fluency-systems-filled/48/user.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
